ea4cff5c75e9b6715ffb730c2d6ef2b874007bc5
ar <ar@6b03482b-4ef8-0310-878d-8b81e9ef6eed>
2005-11-04 13:49:32 +0000

    test bundle

---
+++ test/src/org/knopflerfish/test_bundle/Activator.java
@@ -0,0 +1,22 @@
+/*
+ * Created on Fri Nov 04 14:41:16 CET 2005
+ */
+package org.knopflerfish.test_bundle;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+  
+  /* (non-Javadoc)
+   * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+   */
+  public void start(BundleContext context) throws Exception {
+  }
+
+  /* (non-Javadoc)
+   * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+   */
+  public void stop(BundleContext context) throws Exception {
+  }
+}
