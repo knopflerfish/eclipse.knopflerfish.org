c383d6d5589620164c88bf7daa40d448bbd372fe
ar <ar@6b03482b-4ef8-0310-878d-8b81e9ef6eed>
2011-09-19 19:16:40 +0000

    Bug fix : Clear Bundle Cache in run configuration did not work on eclipse 3.7. Bumped version to 1.1.2

--- org.knopflerfish.eclipse.core.ui/src/org/knopflerfish/eclipse/core/ui/launcher/CustomJavaArgumentsTab.java
+++ org.knopflerfish.eclipse.core.ui/src/org/knopflerfish/eclipse/core/ui/launcher/CustomJavaArgumentsTab.java
@@ -1,14 +1,70 @@
 package org.knopflerfish.eclipse.core.ui.launcher;
 
-import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.jdt.launching.ExecutionArguments;
 
 public class CustomJavaArgumentsTab extends org.eclipse.jdt.debug.ui.launchConfigurations.JavaArgumentsTab
 {
   /**
    * Overrided to call initalizeFrom on all widgets.
    */
+  /*
   public void activated(ILaunchConfigurationWorkingCopy workingCopy) {
     initializeFrom(workingCopy);
   }
+  */
 
+  public void setInitFlag(boolean b)  {
+    String programArgs = getAttributeValueFrom(fPrgmArgumentsText);
+    if (programArgs == null) {
+      programArgs = "";
+    }
+    boolean programArgsChanged = false;
+    ExecutionArguments execArgs = new ExecutionArguments("", programArgs);
+    List argsList = new ArrayList(Arrays.asList(execArgs.getProgramArgumentsArray()));
+    if (!argsList.contains("-init")) {
+      if (b) {
+        argsList.add(0, "-init");
+        programArgsChanged = true;
+      }
+    } else {
+      if (!b) {
+        while(argsList.contains("-init")) {
+          argsList.remove("-init");
+          programArgsChanged = true;
+        }
+      }
+    }
+    if (programArgsChanged) {
+      StringBuffer buf = new StringBuffer();
+      for (Iterator i=argsList.iterator(); i.hasNext(); ) {
+        String a = (String) i.next();
+        if (buf.length() > 0) {
+          buf.append(" ");
+        }
+        buf.append(a);
+      }
+      if (buf.length() == 0) {
+        fPrgmArgumentsText.setText("");
+      } else {
+        fPrgmArgumentsText.setText(buf.toString());
+      }
+    }
+    
+  }
+
+  public boolean getInitFlag()  {
+    String programArgs = getAttributeValueFrom(fPrgmArgumentsText);
+    if (programArgs != null) {
+      ExecutionArguments execArgs = new ExecutionArguments("", programArgs);
+      List argsList = new ArrayList(Arrays.asList(execArgs.getProgramArgumentsArray()));
+      return argsList.contains("-init");
+    } else {
+      return false;
+    }
+  }
 }
